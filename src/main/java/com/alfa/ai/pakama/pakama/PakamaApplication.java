64ac5dadda24786ec20876aa65c7a0d271e6fe97
s.development <s.development2015@gmail.com>
2021-08-21 00:38:52 +0300

    Initial commit

---
+++ src/main/java/com/alfa/ai/pakama/pakama/PakamaApplication.java
@@ -0,0 +1,13 @@
+package com.alfa.ai.pakama.pakama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PakamaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PakamaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alfa/ai/pakama/pakama/PakamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alfa.ai.pakama.pakama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PakamaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
